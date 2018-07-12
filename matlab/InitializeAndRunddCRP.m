503340db92dcf942a0b27eae846dcd176ecdc1c5
kristianeschenburg <keschenb@uw.edu>
2018-07-11 17:20:34 -0700

    git ignore logs

--- matlab/InitializeAndRunddCRP.m
+++ matlab/InitializeAndRunddCRP.m
@@ -8,6 +8,7 @@
 %   Returns the MAP parcellation map_z, as well as a stats objects with
 %   information about the iterations of the model
 function [map_z, stats] = InitializeAndRunddCRP(Z, D_norm, adj_list, sizes, alpha, kappa, nu, sigsq, pass_limit, gt_z, verbose)
+
 % Standard alpha = 10, kappa = 0.0001, nu = 1
 
 % Find highest-probability greedy parcellation to initialize
